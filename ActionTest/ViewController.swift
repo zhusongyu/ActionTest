97b56d40c8d7a692a62da16fa4411ce72068b3db
朱宋宇 <>
2020-01-15 16:23:42 +0800

    first commit

---
+++ ActionTest/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ActionTest
+//
+//  Created by 朱宋宇 on 2020/1/15.
+//  Copyright © 2020 zsy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
